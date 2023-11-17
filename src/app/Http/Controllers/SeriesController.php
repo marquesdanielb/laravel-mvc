88ecf41a360fb9c1b5a585e190b1875e98815cfa
Daniel Marques Barbosa <marquesdanielb@gmail.com>
2023-11-17 13:01:40 -0300

    Create migration, model and views

--- src/app/Http/Controllers/SeriesController.php
+++ src/app/Http/Controllers/SeriesController.php
@@ -2,17 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $series = [
-            'star trek',
-            'naruto',
-            'hunterXhunter'
-        ];
+        $series = Serie::all();
 
         return view('series.index')->with('series', $series);
     }
@@ -20,5 +17,15 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create'); 
+    }
+
+    public function store(Request $request)
+    {
+        $nome = $request->input('nome');
+        $serie = new Serie();
+        $serie->name = $nome;
+        $serie->save();
+
+        return redirect('/series');
     }
 }
--- src/resources/views/series/create.blade.php
+++ src/resources/views/series/create.blade.php
@@ -1,5 +1,6 @@
 <x-layout title="Nova Série">
     <form action="" method="post">
+    @csrf 
         <div class="mb-3">
             <label for="nome" class="form-label">Nome:</label>
             <input type="text" name="nome" id="nome" class="form-control">
