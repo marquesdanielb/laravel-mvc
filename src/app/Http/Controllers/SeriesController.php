c9b27c966bfc4950938f43887fafb37d1bf8cd02
Daniel Marques Barbosa <marquesdanielb@gmail.com>
2023-12-05 12:03:52 -0300

    Edita series

--- src/app/Http/Controllers/SeriesController.php
+++ src/app/Http/Controllers/SeriesController.php
@@ -39,4 +39,18 @@ class SeriesController extends Controller
         return to_route('series.index')
                 ->with('mensagem.sucesso', "Série {$series->name} removida com sucesso");
     }
+
+    public function edit(Serie $series)
+    {
+        return view('series.edit')->with('serie', $series);
+    }
+
+    public function update(Serie $series, Request $request)
+    {
+        $series->name = $request->nome;
+        $series->save();
+
+        return to_route('series.index')
+            ->with('mensagem.sucesso', "Série '{$series->nome}' atualizada com sucesso.");
+    }
 }
--- src/resources/views/series/index.blade.php
+++ src/resources/views/series/index.blade.php
@@ -11,14 +11,19 @@
         @foreach ($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-items-center">
                 {{ $serie->name }}
-                <form action="{{ route('series.destroy', $serie->id) }}" 
-                    method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-danger btn-sm">
-                        x
-                    </button>
-                </form>
+                <span class="d-flex">
+                    <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">
+                        E
+                    </a>
+                    <form action="{{ route('series.destroy', $serie->id) }}" 
+                        class="ms-2" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger btn-sm">
+                            x
+                        </button>
+                    </form>
+                </span>
             </li>
         @endforeach
     </ul>
