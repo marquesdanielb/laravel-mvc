eb0a9a836757ddf084e6180c8eda62e09274a250
Daniel Marques Barbosa <marquesdanielb@gmail.com>
2023-12-07 17:42:21 -0300

    Aplica metodos de autenticacao nas views

--- src/resources/views/components/layout.blade.php
+++ src/resources/views/components/layout.blade.php
@@ -10,7 +10,12 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a href="#" class="navbar-brand">E ai, mano!</a>
-            <a href="{{ route('logout') }}">Sair</a>
+            @auth
+                <a href="{{ route('logout') }}">Sair</a>
+            @endauth
+            @guest
+                <a href="{{ route('login') }}">Entrar</a>
+            @endguest
         </div>
     </nav>
     <div class="container">
--- src/resources/views/seasons/index.blade.php
+++ src/resources/views/seasons/index.blade.php
@@ -2,9 +2,9 @@
     <ul class="list-group">
         @foreach ($seasons as $season)
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                <a href="{{ route('episodes.index', $season->id) }}">
+                @auth<a href="{{ route('episodes.index', $season->id) }}">
                     Temporada: {{ $season->number }}
-                </a>
+                @endauth</a>
                 <span class="badge bg-secondary">
                     {{ $season->numberOfWatchedEpisodes() }} / {{ $season->episodes->count() }}
                 </span>
